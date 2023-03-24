8c30a6709eea023a3ee8b789131f01ef3fb5a58c
mounyaze <mounya.zekraoui@enim.ac.ma>
2023-03-24 13:52:46 +0000

    ..

--- FoodApp/Program.cs
+++ FoodApp/Program.cs
@@ -4,7 +4,10 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddRazorPages();
+
+
 
 builder.Services.AddDbContextPool<FoodAppContext>(options => {
     options.UseSqlServer(builder.Configuration.GetConnectionString("FoodAppCore"));
